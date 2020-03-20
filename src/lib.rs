29aac9c83a1c04231f5a37235a379ed74b15b130
AlbertGoma <58812649+AlbertGoma@users.noreply.github.com>
2020-03-20 22:43:18 +0000

    Base struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use core::ptr::NonNull;
+use ahash::RandomState;
+use hashbrown::raw::RawTable;
+
+
+type Link<K, V> = Option<NonNull<Node<K, V>>>;
+pub type DefaultHashBuilder = RandomState;
+
+
+struct Node<K, V>{
+	k: K,
+	v: V,
+	p: Link<K, V>,
+	n: Link<K, V>,
+}
+
+/// An asynchronous cache for [gws](https://github.com/AlbertGoma/gws)
+/// using [hashbrown](https://github.com/rust-lang/hashbrown).
+pub struct GWSCache<K, V, S = DefaultHashBuilder> {
+    pub(crate) hash_builder: S,
+    pub(crate) table: RawTable<Node<K, V>>,
+    pub(crate) head: Link<K, V>,
+    pub(crate) tail: Link<K, V>,
 }
