10ad1decd70ad8f197c218d6da68816d33cfc226
AlbertGoma <58812649+AlbertGoma@users.noreply.github.com>
2020-03-20 23:04:12 +0000

    Added constructors

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,38 @@ struct Node<K, V>{
 /// An asynchronous cache for [gws](https://github.com/AlbertGoma/gws)
 /// using [hashbrown](https://github.com/rust-lang/hashbrown).
 pub struct GWSCache<K, V, S = DefaultHashBuilder> {
-    pub(crate) hash_builder: S,
-    pub(crate) table: RawTable<Node<K, V>>,
-    pub(crate) head: Link<K, V>,
-    pub(crate) tail: Link<K, V>,
+	pub(crate) hash_builder: S,
+	pub(crate) table: RawTable<Node<K, V>>,
+	pub(crate) head: Link<K, V>,
+	pub(crate) tail: Link<K, V>,
+}
+
+impl<K, V> GWSCache<K, V, DefaultHashBuilder> {
+	pub fn new() -> Self {
+		Self::with_hasher(DefaultHashBuilder::default())
+	}
+	
+	pub fn with_capacity(capacity: usize) -> Self {
+		Self::with_capacity_and_hasher(capacity, DefaultHashBuilder::default())
+	}
+}
+
+impl<K, V, S> GWSCache<K, V, S> {
+	pub fn with_hasher(hash_builder: S) -> Self {
+		Self {
+			hash_builder,
+			table: RawTable::new(),
+			head: None,
+			tail: None,
+		}
+	}
+	
+	pub fn with_capacity_and_hasher(capacity: usize, hash_builder: S) -> Self {
+		Self {
+			hash_builder,
+			table: RawTable::with_capacity(capacity),
+			head: None,
+			tail: None,
+		}
+	}
 }
