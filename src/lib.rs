f964559df1af14c324d4831fb2eb5f487ebf4cca
AlbertGoma <58812649+AlbertGoma@users.noreply.github.com>
2020-03-22 03:29:29 +0000

    Added pop_back() and the push_pop() test. Fixed push_front().

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
-use core::{ptr::NonNull, hash::{BuildHasher, Hash, Hasher}, mem};
+use core::{
+	ptr::NonNull,
+	hash::{
+		BuildHasher,
+		Hash,
+		Hasher
+	},
+	mem,
+	fmt::Debug,
+	//marker::PhantomData
+};
 use ahash::RandomState;
 use hashbrown::raw::RawTable;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -7,12 +17,18 @@ use std::sync::atomic::{AtomicBool, Ordering};
 type Link<K, V> = Option<NonNull<Node<K, V>>>;
 pub type DefaultHashBuilder = RandomState;
 
-
-pub struct Node<K, V>{
+#[derive(Debug)]
+pub struct Node<K, V> {
 	k: K,
 	v: V,
 	p: Link<K, V>,
 	n: Link<K, V>,
+}
+
+impl<K, V> Node<K, V> {
+	fn new(k: K, v: V) -> Self {
+		Self { k, v, p: None, n: None }
+	}
 }
 
 //TODO: impl Drop for Node?
@@ -26,6 +42,7 @@ pub struct GWSCache<K, V, S = DefaultHashBuilder> { //FIXME: where K, V, Send/Sy
 	pub(crate) tail: Link<K, V>,
 	pub(crate) lock: AtomicBool,
 	pub(crate) bytes: usize,
+	//marker: PhantomData<Box<Node<K, V>>>, //necessary?
 }
 
 
@@ -51,6 +68,7 @@ impl<K, V, S> GWSCache<K, V, S> {
 			tail: None,
 			lock: AtomicBool::new(false),
 			bytes: 0,
+			//marker: PhantomData,
 		}
 	}
 
@@ -62,6 +80,7 @@ impl<K, V, S> GWSCache<K, V, S> {
 			tail: None,
 			lock: AtomicBool::new(false),
 			bytes: 0,
+			//marker: PhantomData,
 		}
 	}
 
@@ -80,49 +99,103 @@ impl<K, V, S> GWSCache<K, V, S> {
 
 impl<K, V, S> GWSCache<K, V, S>
 where
-	K: Eq + Hash,
+	K: Eq + Hash + Debug, 
+	V: Debug,
 	S: BuildHasher,
 {
 
 	/// Inserts a key-value pair into the cache at the head of the list 
 	/// and returns the old value if there is one.
 	pub async fn push_front(&mut self, k: K, v: V) -> Option<V> {
-
-		let hash = make_hash(&self.hash_builder, &k);
-		let mut node = Node { k, v, p: None, n: None };
-		let ptr: *mut Node<K, V> = &mut node;
+		let hb = &self.hash_builder;
+		let hash = make_hash(hb, &k);
 		let ret: Option<V>;
 		
+		//Lock:
 		while self.lock.compare_and_swap(false, true, Ordering::Acquire) {}
-			//TODO: increment self.bytes
-			
-			if let Some(mut head) = self.head {
-				node.n = Some(head);
-				unsafe {
-					head.as_mut().p = NonNull::new(ptr);
+			unsafe {
+				let ptr: *mut Node<K, V>;
+
+				//TODO: increment self.bytes
+
+				//Find in HashMap and replace value or insert new node:
+				if let Some(item) = self.table.find(hash, |x| k.eq(&x.k)) {
+					ret = Some(mem::replace(&mut item.as_mut().v, v));
+					ptr = item.as_ptr();
+				} else {
+					ptr = self.table.insert(hash, Node::new(k, v), |x| make_hash(hb, &x.k)).as_ptr();
+					ret = None;
 				}
-				self.head = NonNull::new(ptr);
-			} else {
-				self.head = NonNull::new(ptr);
-				self.tail = NonNull::new(ptr);
+				
+				//Move node to head:
+				if let Some(mut head) = self.head {	//-> One or more entries in cache
+					if head.as_ptr() != ptr {					//-> Node not alredy in head
+						(&mut *ptr).n = self.head;
+						head.as_mut().p = NonNull::new(ptr);
+						self.head = NonNull::new(ptr);
+					}
+				} else {														//-> Empty cache
+					self.head = NonNull::new(ptr);
+					self.tail = NonNull::new(ptr);
+				}
+				
+				#[cfg(debug_assertions)]
+				println!("Head=({:?})\t{:?}\nTail=({:?})\t{:?}\n",
+					self.head.unwrap().as_ptr(),
+					self.head.unwrap().as_ref(),
+					self.tail.unwrap().as_ptr(),
+					self.tail.unwrap().as_ref());
 			}
 
-			if let Some(item) = self.table.find(hash, |x| node.k.eq(&x.k)) {
-				unsafe {
-					ret = Some(mem::replace(&mut item.as_mut().v, node.v));
-				}
-			} else {
-				let hash_builder = &self.hash_builder;
-				self.table.insert(hash, node, |x| make_hash(hash_builder, &x.k));
-				ret = None;
-			}
-			
+		//Unlock:
 		self.lock.store(false, Ordering::Release);
-		
 		ret
 	}
 	
-	//TODO: pub pop_back
+	
+	/// Returns the key and value of the Least Frequently Used item in the
+	/// cache unless it's empty.
+	pub async fn pop_back(&mut self) -> Option<(K, V)> {
+		let ret: Option<(K, V)>;
+		
+		//Lock:
+		while self.lock.compare_and_swap(false, true, Ordering::Acquire) {}
+			if let Some(ptr) = self.tail {
+				unsafe {
+					
+					#[cfg(debug_assertions)]
+					println!("Head=({:?})\t{:?}\nTail=({:?})\t{:?}\n",
+						self.head.unwrap().as_ptr(),
+						self.head.unwrap().as_ref(),
+						self.tail.unwrap().as_ptr(),
+						self.tail.unwrap().as_ref());
+			
+					//Rescue from hashmap:
+					let hash = make_hash(&self.hash_builder, &ptr.as_ref().k);
+					if let Some(item) = self.table.find(hash, |x| ptr.as_ref().k.eq(&x.k)) {
+						self.table.erase_no_drop(&item);
+	
+						//Set new tail:
+						self.tail = ptr.as_ref().p;
+						match self.tail {
+							None => self.head = None,
+							Some(t) => (*t.as_ptr()).n = None,
+						}
+						let node = item.read();
+						ret = Some((node.k, node.v));
+						//TODO: drop node?
+					} else {
+						ret = None;
+					}
+				}
+			} else {
+				ret = None;
+			}
+		
+		//Unlock:
+		self.lock.store(false, Ordering::Release);
+		ret
+	}
 	
 	//TODO: move_front
 	
@@ -137,5 +210,7 @@ pub(crate) fn make_hash<K: Hash + ?Sized>(hash_builder: &impl BuildHasher, val: 
 }
 
 //TODO: impl Drop for GWSCache?
+
+//TODO: impl Sync for GWSCache
 
 
--- tests/gws-cache.rs
+++ tests/gws-cache.rs
@@ -1,4 +1,5 @@
 use gws_cache::{GWSCache, DefaultHashBuilder};
+use futures::executor::block_on;
 
 
 #[test]
@@ -18,4 +19,24 @@ fn new_cache() {
 	assert_eq!(t.capacity(), 0);
 }
 
-//TODO: test push_front
+#[test]
+fn push_pop() {
+	type GWSC = GWSCache<u8, &'static str>;
+	
+	let mut c = GWSC::new();
+	block_on(c.push_front(1, "This"));
+	block_on(c.push_front(2, "is"));
+	assert_eq!(c.len(), 2);
+	block_on(c.push_front(3, "a"));
+	block_on(c.push_front(4, "function"));
+	block_on(c.push_front(4, "test"));
+	assert_eq!(c.len(), 4);
+
+	assert_eq!(block_on(c.pop_back()), Some((1, "This")));
+	assert_eq!(block_on(c.pop_back()), Some((2, "is")));
+	assert_eq!(block_on(c.pop_back()), Some((3, "a")));
+	assert_eq!(block_on(c.pop_back()), Some((4, "test")));
+	assert_eq!(block_on(c.pop_back()), None);
+}
+
+//TODO: test concurrent!
